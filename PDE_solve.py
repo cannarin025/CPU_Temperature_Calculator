8068d435ac519cff65211315728e177d389ab3e1
Can Narin <cannarin025@gmail.com>
2020-12-16 01:25:38 +0000

    Jacobi solve and BCs implemented. Jacobi solve converges incorrectly. Probably due to update procedure (fix).

--- PDE_solve.py
+++ PDE_solve.py
@@ -1,60 +1,127 @@
 import numpy as np
 
 
-def Poisson_Solve(y_range, x_range):
-    # remember numpy matrices are indexed: matrix[row][col]
-    u = np.zeros((y_range, x_range))  # matrix over all space
-    print(u)
-    b = np.zeros((y_range, 1))
-    print(b)
-    laplacian_mat = np.zeros((y_range, x_range))
-
-    for i in range(x_range): #rows
-        for j in range(y_range): #cols
-            if i != 0 and i != y_range - 1 and j != 0 and j != x_range:
-                laplacian_mat[i][j] = u[i - 1][j] + u[i][j - 1] + u[i + 1][j] + u[i][j + 1] - 4 * u[i][j]
-
-    return
-
-
-def Jacobi_Solve(A, b, guess = -1, iterations = 1):
-    #var definitions
-    if guess == -1:
-        guess = np.zeros((np.shape(A)[0], 1))
-        print(guess, "guess")
-        print()
-
-    if np.shape(A)[0] == np.shape(A)[1]:
-        diagonal = np.zeros(np.shape(A))
-        T = np.zeros(np.shape(A))
-
-        for i in range(np.shape(A)[0]): #rows
-            for j in range(np.shape(A)[1]): #cols
-                if i == j:
-                    diagonal[i,j] = A[i,j]
-
-                elif i != j:
-                    T[i,j] = A[i,j]
-
-        print(diagonal, "diagonal")
-        print()
-        print(T,"T")
-        #getting diagonal and T
-        for n in range(iterations):
-            diagonal_inv = np.linalg.inv(diagonal)
-            next_guess = -1 * np.matmul(diagonal_inv, np.matmul(T, guess)) + np.matmul(diagonal_inv, b)
-            guess = next_guess
-
-        x = guess
-
-        return x
-
-
+def H_Natural(surf_temp, amb_temp):
+    if surf_temp - amb_temp >= 0:
+        return np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
     else:
-        print("Error, input matrix not square!")
+        return -1 * np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
+
+
+def H_Forced(wind_speed):
+    return 11.4 + 5.7 * wind_speed
+
+
+def Neumann_Boundaries(initial_state, hx, hy, amb_temp, wind_speed, k, natural=True):
+    # setting boundary conditions
+
+    initial_x_dim = np.shape(initial_state)[1]
+    initial_y_dim = np.shape(initial_state)[0]
+
+    for i in range(initial_y_dim):
+        for j in range(initial_x_dim):
+            if natural:
+                # if i == 0:  # bottom boundary
+                #     surf_temp = initial_state[i + 1, j]
+                #     H = H_Natural(surf_temp, amb_temp)
+                #     initial_state[i, j] = surf_temp - 2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)
+                # if i == initial_y_dim - 1:  # top boundary
+                #     surf_temp = initial_state[i - 1, j]
+                #     initial_state[i, j] = surf_temp - 2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)
+                # if j == 0:  # left boundary
+                #     surf_temp = initial_state[i, j + 1]
+                #     initial_state[i, j] = surf_temp - 2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)
+                # if j == initial_x_dim - 1:  # right boundary
+                #     surf_temp = initial_state[i, j - 1]
+                #     initial_state[i, j] = surf_temp - 2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)
+
+                if i == 0:  # bottom boundary
+                    surf_temp = initial_state[i + 1, j]
+                    H = H_Natural(surf_temp, amb_temp)
+                    initial_state[i, j] = surf_temp - ((2 * hx * H_Natural(surf_temp, amb_temp) * (amb_temp - surf_temp)) / -k)
+                if i == initial_y_dim - 1:  # top boundary
+                    surf_temp = initial_state[i - 1, j]
+                    initial_state[i, j] = surf_temp - ((2 * hx * H_Natural(surf_temp, amb_temp) * (amb_temp - surf_temp)) / -k)
+                if j == 0:  # left boundary
+                    surf_temp = initial_state[i, j + 1]
+                    initial_state[i, j] = surf_temp - ((2 * hy * H_Natural(surf_temp, amb_temp) * (amb_temp - surf_temp)) / -k)
+                if j == initial_x_dim - 1:  # right boundary
+                    surf_temp = initial_state[i, j - 1]
+                    initial_state[i, j] = surf_temp - ((2 * hy * H_Natural(surf_temp, amb_temp) * (amb_temp - surf_temp)) / -k)
+
+                # if i == 0:  # bottom boundary
+                #     surf_temp = initial_state[i + 1, j]
+                #     H = H_Natural(surf_temp, amb_temp)
+                #     initial_state[i, j] = surf_temp - ((2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)) / -k)
+                # if i == initial_y_dim - 1:  # top boundary
+                #     surf_temp = initial_state[i - 1, j]
+                #     initial_state[i, j] = surf_temp - ((2 * hx * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)) / -k)
+                # if j == 0:  # left boundary
+                #     surf_temp = initial_state[i, j + 1]
+                #     initial_state[i, j] = surf_temp - ((2 * hy * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)) / -k)
+                # if j == initial_x_dim - 1:  # right boundary
+                #     surf_temp = initial_state[i, j - 1]
+                #     initial_state[i, j] = surf_temp - ((2 * hy * H_Natural(surf_temp, amb_temp) * (surf_temp - amb_temp)) / -k)
+
+            elif not natural:
+                if i == 0:  # bottom boundary
+                    surf_temp = initial_state[i + 1, j]
+                    initial_state[i, j] = surf_temp - 2 * hx * H_Forced(wind_speed) * (surf_temp - amb_temp)
+                if i == initial_y_dim - 1:  # top boundary
+                    surf_temp = initial_state[i - 1, j]
+                    initial_state[i, j] = surf_temp - 2 * hx * H_Forced(wind_speed) * (surf_temp - amb_temp)
+                if j == 0:  # left boundary
+                    surf_temp = initial_state[i, j + 1]
+                    initial_state[i, j] = surf_temp - 2 * hx * H_Forced(wind_speed) * (surf_temp - amb_temp)
+                if j == initial_x_dim - 1:  # right boundary
+                    surf_temp = initial_state[i, j - 1]
+                    initial_state[i, j] = surf_temp - 2 * hx * H_Forced(wind_speed) * (surf_temp - amb_temp)
+
+    return initial_state
+
+def Jacobi_Solve(x_range, y_range, x_points = 10, y_points = 10, initial_guess = 0, natural = True):
+    #definitions
+    hx = x_range / x_points
+    hy = y_range / y_points
+
+    initial_x_dim = x_points + 2
+    initial_y_dim = y_points + 2
+    initial_state = np.full((initial_y_dim, initial_x_dim), initial_guess)
+
+    #initial boundary conditions
+    amb_temp = 20
+    wind_speed = 30
+
+    for iteration in range(10000):
+        final_state = np.zeros((initial_y_dim, initial_x_dim))
+        initial_state = Neumann_Boundaries(initial_state, hx, hy, amb_temp, wind_speed, 150000,natural)
+
+        #solving iteration of Jacobi method
+        for i in range(1, initial_y_dim - 1): #y
+            for j in range(1, initial_x_dim - 1): #x
+                # s = #this formula is incomplete without the S. What is S?
+                updated_value = (((1/(hx**2)) * initial_state[i+1, j]) + ((1/(hx**2)) * initial_state[i-1, j]) + ((1/(hy**2)) * initial_state[i, j-1]) + ((1/(hy**2)) * initial_state[i, j+1])) / ((2/(hx**2)) + (2/(hy**2)))
+                final_state[i,j] = updated_value
+
+        initial_state = final_state
+
+
+    #tidies up array
+    final_state = np.delete(final_state, 0, 0)
+    final_state = np.delete(final_state, 0, 1)
+    final_state = np.delete(final_state, initial_y_dim - 2, 0)
+    final_state = np.delete(final_state, initial_x_dim - 2, 1)
+
+    print()
+    print(final_state)
+            
+
+Jacobi_Solve(0.014,0.002, x_points= 10, y_points=10, initial_guess=8600)
+
+
 
 #Poisson_Solve(5, 5)
 
-a = np.matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
-
-x = Jacobi_Solve(a,a,-1,1)
+a = np.matrix([[0.1,0.2,0.3,0.4],[0.5,0.6,0.7,0.8],[0.9,0.111,0.11,0.12],[0.13,0.14,0.15,0.16]])
+b = np.matrix([[5,-2,3],[-3,9,1],[2,-1,-7]])
+c = np.matrix([[-1],[2],[3]])
