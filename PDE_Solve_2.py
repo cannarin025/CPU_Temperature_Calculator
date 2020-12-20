Working on standard jacobi method in Main_Method.py and heat based approach in Heat_transfer_method.py. Main_Method.py works as expected but converges to wrong value with power.

PDE_Solve_2.py
@@ -6,8 +6,8 @@ def H_Natural(surf_temp, amb_temp):
     if surf_temp - amb_temp >= 0:
         return np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
     else:
-        return -1 * np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
-
+        #return -1 * np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
+        return np.abs(1.31 * ((float(surf_temp) - amb_temp) ** (1 / 3)))
 
 def H_Forced(wind_speed):
     return 11.4 + 5.7 * wind_speed
@@ -34,7 +34,7 @@ def Neumann_Boundaries(initial_state, hx, hy, amb_temp, wind_speed, k, natural=T
 
                 """Central Difference"""
 
-                #this seems to work. Do not know why.
+                # this seems to work. Do not know why.
                 # if i == 0:  # bottom boundary
                 #     surf_temp = initial_state[i + 1, j]
                 #     initial_state[i, j] = initial_state[i + 2, j] - ((2 * hx * H_Natural(surf_temp, amb_temp) * (amb_temp - surf_temp)) / -k)
@@ -51,16 +51,26 @@ def Neumann_Boundaries(initial_state, hx, hy, amb_temp, wind_speed, k, natural=T
                 #central difference calculated by hand (does not work as the one above does)
                 if i == 0:                  #bottom
                     surf_temp = initial_state[i + 1, j]
-                    initial_state[i,j] = initial_state[i + 2, j] - 2 * hy * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
+                    initial_state[i,j] = initial_state[i + 2, j] + 2 * hy * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
                 if i == initial_y_dim - 1:  #top
                     surf_temp = initial_state[i - 1, j]
                     initial_state[i,j] = initial_state[i - 2, j] + 2 * hy * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
                 if j == 0:                  #left
                     surf_temp = initial_state[i, j + 1]
-                    initial_state[i,j] = initial_state[i, j + 2] - 2 * hx * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
+                    initial_state[i,j] = initial_state[i, j + 2] + 2 * hx * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
                 if j == initial_x_dim - 1:  #right
                     surf_temp = initial_state[i, j - 1]
                     initial_state[i,j] = initial_state[i, j - 2] + 2 * hx * (-1 * (Phi_s(surf_temp, amb_temp, natural=natural))/k)
+
+                #central difference with boundaries set to Ta.
+                # if i == 0:                  #bottom
+                #     initial_state[i + 1, j] = (k * (initial_state[i + 2, j] - amb_temp)/(-2*H_Natural()))
+                # if i == initial_y_dim - 1:  #top
+                #
+                # if j == 0:                  #left
+                #
+                # if j == initial_x_dim - 1:  #right
+
 
                 """Forward Difference"""
 
@@ -107,8 +117,8 @@ def Jacobi_Solve(x_range, y_range, x_points = 10, y_points = 10, initial_guess =
     amb_temp = 20
     wind_speed = 30
     k = 150
-    #q = 0.5e9
-    q = 0
+    q = 0.5e9
+    #q = 0
 
     for iteration in range(10000):
         final_state = np.zeros((initial_y_dim, initial_x_dim))
@@ -117,11 +127,14 @@ def Jacobi_Solve(x_range, y_range, x_points = 10, y_points = 10, initial_guess =
         #solving iteration of Jacobi method
         for i in range(1, initial_y_dim - 1): #y
             for j in range(1, initial_x_dim - 1): #x
+                c = 1 / (2 * ((hy ** 2) + (hx ** 2)))
                 s = - q / k
+                hy2 = np.square(hy)
+                hx2 = np.square(hx)
                 #updated_value = (((1/(hx**2)) * initial_state[i+1, j]) + ((1/(hx**2)) * initial_state[i-1, j]) + ((1/(hy**2)) * initial_state[i, j-1]) + ((1/(hy**2)) * initial_state[i, j+1]) - s) / ((2/(hx**2)) + (2/(hy**2)))
-                c = 1/(2*((hy**2) + (hx**2)))
                 #updated_value = c * (((hy**2) * initial_state[i - 1, j]) + ((hy**2) * initial_state[i + 1, j]) + ((hx**2) * initial_state[i, j + 1]) + ((hx**2) * initial_state[i, j - 1]) - ((hx**2) * (hy**2) * s))
-                updated_value = c * (((hx**2) * (initial_state[i-1,j] + initial_state[i+1,j])) + ((hy**2) * (initial_state[i,j-1] + initial_state[i,j+1])) - ((hy**2) * (hx**2) * s))
+                #updated_value = c * (((hx**2) * (initial_state[i-1,j] + initial_state[i+1,j])) + ((hy**2) * (initial_state[i,j-1] + initial_state[i,j+1])) - ((hy**2) * (hx**2) * s))
+                updated_value = c * (hy2 * (initial_state[i, j - 1] + initial_state[i, j + 1]) + hx2 * (initial_state[i - 1, j] + initial_state[i + 1, j]) - hx2 * hy2 * s)
                 final_state[i,j] = updated_value
                 a = 1
         initial_state = final_state
@@ -133,10 +146,12 @@ def Jacobi_Solve(x_range, y_range, x_points = 10, y_points = 10, initial_guess =
     final_state = np.delete(final_state, initial_y_dim - 2, 0)
     final_state = np.delete(final_state, initial_x_dim - 2, 1)
 
+    avg_temp = np.average(final_state)
     print()
     print(final_state)
+    print(avg_temp)
     return final_state
 
 
-Jacobi_Solve(0.014,0.002, x_points= 10, y_points=10, initial_guess=5)
+Jacobi_Solve(0.014, 0.001, x_points= 10, y_points=10, initial_guess=20)
 