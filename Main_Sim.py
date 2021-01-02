9b4d5db591a9187f3725676ea44ea06512144605
Can Narin <cannarin025@gmail.com>
2021-01-02 15:31:27 +0000

    Heat-sink boundaries fixed. New bug where temperature is skewed to left side for unknown reason.

--- Main_Sim.py
+++ Main_Sim.py
@@ -1,17 +1,18 @@
 from Lib.Simulation import Simulation
 
-sim = Simulation(grid_spacing=0.5, initial_guess=20) #cannot set grid spacing to 0.5 for some reason (defaults to 0.1)
+sim = Simulation(grid_spacing=0.1, initial_guess=3000) #cannot set grid spacing to 0.5 for some reason (defaults to 0.1)
 
 sim.add_processor(name="CPU", x_dim=14, y_dim=2)
 sim.add_ceramic(name = "Casing", x_dim=20, y_dim=4)
-sim.add_heat_sink(name="HeatSink", n_fins=5, fin_height=5, fin_width=2, fin_spacing=2)
+sim.add_heat_sink(name="HeatSink", n_fins=8, fin_height=5, fin_width=2, fin_spacing=2)
 
 sim.mount_to_top("CPU", "Casing")
 sim.mount_to_top("Casing", "HeatSink")
 
-sim.jacobi_solve(1000)
+sim.jacobi_solve(300000)
 
 sim.graph_individual()
+sim.save_data()
 
 a = 1
 
